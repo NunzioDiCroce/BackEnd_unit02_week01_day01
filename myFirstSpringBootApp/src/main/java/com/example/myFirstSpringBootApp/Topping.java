67a1b2bb00d1a13b77d962a76a02bc1b95fd61ba
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-24 16:41:06 +0200

    Create Topping.java

---
+++ myFirstSpringBootApp/src/main/java/com/example/myFirstSpringBootApp/Topping.java
@@ -0,0 +1,17 @@
+package com.example.myFirstSpringBootApp;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+
+public class Topping extends Product {
+	private String name;
+	private String calories;
+
+}
