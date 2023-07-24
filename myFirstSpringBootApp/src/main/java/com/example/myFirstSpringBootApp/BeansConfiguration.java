14fb3024fbb19078a28967f8df0f39381a2443ae
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-24 18:40:20 +0200

    Update BeansConfiguration.java

--- myFirstSpringBootApp/src/main/java/com/example/myFirstSpringBootApp/BeansConfiguration.java
+++ myFirstSpringBootApp/src/main/java/com/example/myFirstSpringBootApp/BeansConfiguration.java
@@ -15,28 +15,48 @@ public class BeansConfiguration {
 	}
 
 	@Bean(name = "Margherita")
-	Pizza getMargherita() {
+	Pizza getMargheritaPizza() {
 		return new Pizza("Margherita Pizza", "tomato", "cheese", 1104, 4.99, PizzaSize.STANDARD);
 	}
 
 	@Bean(name = "Hawaiian")
-	Pizza getHawaiian() {
+	Pizza getHawaiianPizza() {
 		return new Pizza("Hawaiian Pizza", "tomato", "cheese", "ham", "pineapple", 1024, 6.49, PizzaSize.STANDARD);
 	}
 
 	@Bean(name = "Salami")
-	Pizza getSalami() {
+	Pizza getSalamiPiazza() {
 		return new Pizza("Salami Pizza", "tomato", "cheese", "salami", 1160, 5.99, PizzaSize.STANDARD);
 	}
 
 	@Bean(name = "Custom")
-	Pizza getCustom() {
+	Pizza getCustomPizza() {
 		return new Pizza("Custom Pizza", "tomato", "cheese", "salami", "onion", 1104 + 86 + 22, 4.99 + 0.99 + 0.69,
 				PizzaSize.LARGE);
 	}
 
 	@Bean(name = "Cheese")
 	Topping getCheese() {
-		return new Topping("Cheese", 92.0, 0.69);
+		return new Topping("Cheese", 92, 0.69);
+	}
+
+	@Bean(name = "Ham")
+	Topping getHam() {
+		return new Topping("Ham", 35, 0.99);
+	}
+
+	@Bean(name = "Onions")
+	Topping getOnions() {
+		return new Topping("Onions", 22, 0.69);
+	}
+
+	@Bean(name = "Pineapple")
+	Topping getPineapple() {
+		return new Topping("Pineapple", 24, 0.79);
+	}
+
+	@Bean(name = "Salami")
+	Topping getSalami() {
+		return new Topping("Salami", 86, 0.99);
 	}
 }
