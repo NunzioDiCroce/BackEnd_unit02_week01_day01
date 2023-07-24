9d1f563adfa054bcebab126f20dbeb44101a113e
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-24 18:22:01 +0200

    Update Franchise.java

--- myFirstSpringBootApp/src/main/java/com/example/myFirstSpringBootApp/Franchise.java
+++ myFirstSpringBootApp/src/main/java/com/example/myFirstSpringBootApp/Franchise.java
@@ -10,7 +10,7 @@ import lombok.ToString;
 @AllArgsConstructor
 @ToString
 
-public class Franchise extends Product {
+public class Franchise {
 	protected String name;
-
+	protected double price;
 }
