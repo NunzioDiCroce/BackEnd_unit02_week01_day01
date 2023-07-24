f7b2e5459b48aeca5fa11b6730bc3a300d1b829b
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-24 16:45:56 +0200

    Create Franchise.java

---
+++ myFirstSpringBootApp/src/main/java/com/example/myFirstSpringBootApp/Franchise.java
@@ -0,0 +1,16 @@
+package com.example.myFirstSpringBootApp;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+
+public class Franchise extends Product {
+	protected String name;
+
+}
