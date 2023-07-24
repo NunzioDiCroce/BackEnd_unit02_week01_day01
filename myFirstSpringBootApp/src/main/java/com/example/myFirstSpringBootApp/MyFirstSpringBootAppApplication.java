ddab2c9779d021bf0b41a067f24f0f3f8912c923
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-24 15:49:40 +0200

    Update MyFirstSpringBootAppApplication.java

--- myFirstSpringBootApp/src/main/java/com/example/myFirstSpringBootApp/MyFirstSpringBootAppApplication.java
+++ myFirstSpringBootApp/src/main/java/com/example/myFirstSpringBootApp/MyFirstSpringBootAppApplication.java
@@ -2,12 +2,20 @@ package com.example.myFirstSpringBootApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class MyFirstSpringBootAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyFirstSpringBootAppApplication.class, args);
+	}
+
+	public static void configWithAnnotationConfing() {
+
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(
+				MyFirstSpringBootAppApplication.class);
+
 	}
 
 }
