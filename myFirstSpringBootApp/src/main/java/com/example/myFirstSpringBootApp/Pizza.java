9d74a389d13f7cc0bdd4b9ff0bba4e05db71c033
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-24 18:20:16 +0200

    Update Pizza.java

--- myFirstSpringBootApp/src/main/java/com/example/myFirstSpringBootApp/Pizza.java
+++ myFirstSpringBootApp/src/main/java/com/example/myFirstSpringBootApp/Pizza.java
@@ -20,29 +20,34 @@ public class Pizza {
 	protected double price;
 	protected PizzaSize pizzaSize;
 
-	public Pizza(String _name, String _firstTopping, String _secondTopping) {
-		super();
+	public Pizza(String _name, String _firstTopping, String _secondTopping, String _calories, double _price,
+			PizzaSize _pizzaSize) {
 		this.name = _name;
 		this.firstTopping = _firstTopping;
 		this.secondTopping = _secondTopping;
+		this.calories = _calories;
+		this.price = _price;
+		this.pizzaSize = _pizzaSize;
 	}
 
-	public Pizza(String _name, String _firstTopping, String _secondTopping, String _thirdTopping) {
-		super();
+	public Pizza(String _name, String _firstTopping, String _secondTopping, String _thirdTopping, String _calories,
+			double _price, PizzaSize _pizzaSize) {
 		this.name = _name;
 		this.firstTopping = _firstTopping;
 		this.secondTopping = _secondTopping;
-		this.thirdTopping = _thirdTopping;
+		this.calories = _calories;
+		this.price = _price;
+		this.pizzaSize = _pizzaSize;
 	}
 
-	public Pizza(String _name, String _firstTopping, String _secondTopping, String _thirdTopping,
-			String _fourthTopping) {
-		super();
+	public Pizza(String _name, String _firstTopping, String _secondTopping, String _thirdTopping, String _fourthTopping,
+			String _calories, double _price, PizzaSize _pizzaSize) {
 		this.name = _name;
 		this.firstTopping = _firstTopping;
 		this.secondTopping = _secondTopping;
-		this.thirdTopping = _thirdTopping;
-		this.fourthTopping = _fourthTopping;
+		this.calories = _calories;
+		this.price = _price;
+		this.pizzaSize = _pizzaSize;
 	}
 
 }
