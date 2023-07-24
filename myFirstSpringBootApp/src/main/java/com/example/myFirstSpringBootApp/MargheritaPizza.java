e191fd0e17932ee8c7f6c1d47fae0d47c9408ba2
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-24 16:34:34 +0200

    Create Product.java

---
+++ myFirstSpringBootApp/src/main/java/com/example/myFirstSpringBootApp/MargheritaPizza.java
@@ -0,0 +1,18 @@
+package com.example.myFirstSpringBootApp;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+
+public class MargheritaPizza extends Product {
+	private String firstTopping;
+	private String secondTopping;
+	private String calories;
+
+}
