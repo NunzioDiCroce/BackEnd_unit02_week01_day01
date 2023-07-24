fe1ef271be177114247bcd3152fd7e526a59a257
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-24 15:19:12 +0200

    Create BeansConfigurationEsercizio1.java

---
+++ myFirstSpringBootApp/src/main/java/com/example/myFirstSpringBootApp/BeansConfigurationEsercizio1.java
@@ -0,0 +1,12 @@
+package com.example.myFirstSpringBootApp;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+
+public class BeansConfigurationEsercizio1 {
+
+	@Bean
+	
+}
