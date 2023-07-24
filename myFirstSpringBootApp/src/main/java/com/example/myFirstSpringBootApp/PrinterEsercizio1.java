e15bc8403514f551c59a5b49d90501077567dc37
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-24 15:14:27 +0200

    Update PrinterEsercizio1.java

--- myFirstSpringBootApp/src/main/java/com/example/myFirstSpringBootApp/PrinterEsercizio1.java
+++ myFirstSpringBootApp/src/main/java/com/example/myFirstSpringBootApp/PrinterEsercizio1.java
@@ -1,5 +1,15 @@
 package com.example.myFirstSpringBootApp;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+
 public class PrinterEsercizio1 implements IEsercizio1 {
 
 	public void print() {
